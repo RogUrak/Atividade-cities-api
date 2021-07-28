ee64ad05d8dfe43574f2d36d088f55a3553bc4a7
RogUrak <rogerio.urakawa@gmail.com>
2021-07-28 18:47:31 -0300

    Commit inicial

---
+++ main/java/com/github/RogUrak/states/repositories/StateRepository.java
@@ -0,0 +1,6 @@
+package com.github.RogUrak.states.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
